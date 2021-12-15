8b33ecd722b7d01cb1a07d5b972f6a63515775cc
resulyag <resulyag@gmail.com>
2021-12-15 23:46:44 +0300

    Category Kategorilerin listelenmesi

--- CoreDemo/Controllers/Category.cs
+++ CoreDemo/Controllers/Category.cs
@@ -2,15 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CoreDemo.Controllers
 {
     public class Category : Controller
     {
+        CategoryManager cm = new CategoryManager(new EfCategoryRepository());
         public IActionResult Index()
         {
-            return View();
+            var values = cm.GetList();
+            return View(values);
         }
     }
 }
