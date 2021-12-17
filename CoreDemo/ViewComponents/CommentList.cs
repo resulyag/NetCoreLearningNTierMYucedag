764506a0dac43ab5b88650504141c0a1f7820b19
resulyag <resulyag@gmail.com>
2021-12-17 17:54:39 +0300

    View Component

---
+++ CoreDemo/ViewComponents/CommentList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.ViewComponents
+{
+    public class CommentList:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentValues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID = 1,
+                    UserName = "Murat"
+                },
+                new UserComment
+                {
+                    ID = 2,
+                    UserName= "Mesut"
+                },
+                new UserComment
+                {
+                    ID =3,
+                    UserName="Merve"
+                }
+            };
+            return View(commentValues);
+        }
+    }
+}
