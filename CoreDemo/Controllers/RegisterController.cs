9b267e1bf4be36b8f68bdd91c4fe3eca62d3a4ff
resulyag <resulyag@gmail.com>
2021-12-18 19:22:37 +0300

    HttpGet Ve HttpPost attributeleri Ve Register işlemleri

---
+++ CoreDemo/Controllers/RegisterController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoreDemo.Controllers
+{
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index()//parametre alacak
+        {
+            return View();
+        }
+    }
+}
