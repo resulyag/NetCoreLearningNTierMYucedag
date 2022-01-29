371b2f6f8b574f4e70297963cb9a2bc9c0e107ce
resulyag <resulyag@gmail.com>
2022-01-29 21:25:44 +0300

    Notification Sınıfı

---
+++ CoreDemo/ViewComponents/Notification/NotificationList.cs
@@ -0,0 +1,19 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDemo.ViewComponents.Notification
+{
+    public class NotificationList:ViewComponent
+    {
+        BlogManager bm = new BlogManager(new EfBlogRepository());
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
