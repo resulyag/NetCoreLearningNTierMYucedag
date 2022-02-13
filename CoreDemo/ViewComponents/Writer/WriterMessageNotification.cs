067ab8a49e6e089f859818d453c1590980c7b408
resulyag <resulyag@gmail.com>
2022-02-13 15:43:13 +0300

    Mesaj Bildirimlerinin Tamamlanması

--- CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +8,14 @@ using System.Threading.Tasks;
 
 namespace CoreDemo.ViewComponents.Writer
 {
-    public class WriterMessageNotification:ViewComponent
+    public class WriterMessageNotification : ViewComponent
     {
+        MessageManager mm = new MessageManager(new EfMessageRepository());
         public IViewComponentResult Invoke()
         {
-            return View();
+            string p = "deneme1@gmail.com";
+            var values = mm.GetInboxListByWriter(p);
+            return View(values);
         }
     }
 }
