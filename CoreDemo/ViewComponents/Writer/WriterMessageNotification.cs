7a0837e25de9e2e351b625d3552152564aa0a36b
resulyag <resulyag@gmail.com>
2022-02-20 02:34:35 +0300

    Yazara Ait Mesaj Bildirimleri

--- CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
+++ CoreDemo/ViewComponents/Writer/WriterMessageNotification.cs
@@ -10,11 +10,11 @@ namespace CoreDemo.ViewComponents.Writer
 {
     public class WriterMessageNotification : ViewComponent
     {
-        MessageManager mm = new MessageManager(new EfMessageRepository());
+        Message2Manager mm = new Message2Manager(new EfMessage2Repository());
         public IViewComponentResult Invoke()
         {
-            string p = "deneme1@gmail.com";
-            var values = mm.GetInboxListByWriter(p);
+            int id = 2;
+            var values = mm.GetInboxListByWriter(id);
             return View(values);
         }
     }
