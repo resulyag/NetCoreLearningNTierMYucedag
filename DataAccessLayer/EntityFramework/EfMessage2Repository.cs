cc6bd875d134fd0b31f9a18eb0ef5aa6f8dc6812
resulyag <resulyag@gmail.com>
2022-02-26 18:05:03 +0300

    Message Include ile sender id si yerine ismi getirme

--- DataAccessLayer/EntityFramework/EfMessage2Repository.cs
+++ DataAccessLayer/EntityFramework/EfMessage2Repository.cs
@@ -6,10 +6,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataAccessLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.EntityFramework
 {
     public class EfMessage2Repository:GenericRepository<Message2>, IMessage2Dal
     {
+        public List<Message2> GetListWithMessageByWriter(int id)
+        {
+            using (var c = new Context())
+            {
+                return c.Message2s.Include(x => x.SenderUser).Where(x => x.ReceiverID == id).ToList();
+            }
+        }
     }
 }
